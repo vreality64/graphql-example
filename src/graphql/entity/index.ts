3d55b7cb36303b5f3383bd4af1f18b1931e27a97
ian.park <ian.park@navercorp.com>
2018-10-15 21:47:25 +0900

    Add extensions option for select only graphql types

--- src/graphql/entity/index.ts
+++ src/graphql/entity/index.ts
@@ -2,12 +2,12 @@ import * as path from "path"
 import { fileLoader, mergeTypes, mergeResolvers } from "merge-graphql-schemas"
 
 // merge all types in child directories
-const typesPath = path.join(__dirname, './')
-const typesArray = fileLoader(typesPath, { recursive: true })
+const typesPath = path.join(__dirname, "./")
+const typesArray = fileLoader(typesPath, { recursive: true, extensions: [".graphql"] })
 const typeDefs = mergeTypes(typesArray, { all: true })
 
 // merge all resolvers in child directories
-const resolversPath = path.join(__dirname, '/**/*.resolver.*')
+const resolversPath = path.join(__dirname, "/**/*.resolver.*")
 const resolversArray = fileLoader(resolversPath)
 const resolvers = mergeResolvers(resolversArray)
 
